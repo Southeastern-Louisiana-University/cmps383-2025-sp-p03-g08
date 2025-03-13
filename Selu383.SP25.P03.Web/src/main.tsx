9fbb93bbba2f88e1372cd35a1bd271e9bd10b157
Holden <Holden.morris@selu.edu>
2025-03-13 12:51:05 -0500

    Mantine Implemented, 404 page done

--- Selu383.SP25.P03.Web/src/main.tsx
+++ Selu383.SP25.P03.Web/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router";
-import { RoutesConfig } from "./routes/config.tsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <RoutesConfig />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
--- Selu383.SP25.P03.Web/src/routes/routeConfig.tsx
+++ Selu383.SP25.P03.Web/src/routes/routeConfig.tsx
@@ -1,13 +1,14 @@
 // Render page when the path matches the specified route
 import { Route, Routes } from "react-router";
 import { routes } from "./routeIndex";
-import App from "../App";
-import HomePage from "../pages/home-page";
+import { NotFoundTitle } from "../pages/page-not-found/not-found";
+import HomePage from "../pages/home-page/home-page";
 
 export const RoutesConfig = () => {
   return (
     <Routes>
-      <Route path={routes.root} element={<App />} />
+      <Route path="*" element={<NotFoundTitle />} />
+      <Route path={routes.root} element={<HomePage />} />
       <Route path={routes.home} element={<HomePage />} />
     </Routes>
   );
